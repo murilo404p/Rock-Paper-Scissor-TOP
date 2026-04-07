1bf83fd2407bfc88c8c1d912b7b4160055da18ee
Murilo Silva Papacidero <mpapacidero3@gmail.com>
2026-04-07 16:09:41 -0300

    função getComputerChoice finalizada

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let humanScore = 0;
-let computerScore = 0;
+// Função para o computador escolher uma jogada aleatória
+function getComputerChoice() {
+    const options = ["rock", "paper", "scissors"]; // Cada valor dentro do array tem seu index
+    const index = Math.floor(Math.random() * options.length); // gera um valor aleatório entre 0 e 2
+    const choice = options[index]; // choice ira guardar o valor 
+    return choice;
+}
 
 function getHumanChoice() {
-
+    
 }
